f3e6dba9144771f9044c61a4555e733b9fde18b5
MinstrelHughes <zhuoran4@illinois.edu>
2018-03-23 16:52:58 -0500

    naive server 2

--- server.js
+++ server.js
@@ -7,50 +7,54 @@ var express    = require('express'),
 
 var connection = mysql.createConnection({
     host: "192.17.90.133",
-    user: "servernotfound_u",
-    password: "laozhuquanxidi1"
+	  user: "servernotfound_test",
+    password: "YauU.f_%Qr6^",
+    database: "servernotfound_data",
+    port: 3306
 });
 
-var app = module.exports = express.createServer();
+// connection.connect(function(err) {
+//   if (err){
+//     consol.log('error ??');
+//     return;
+//   }
+//   console.log("Connected!");
+// });
+
+var app = express();
 
 // Database setup
 
-// connection.query('CREATE DATABASE IF NOT EXISTS test', function (err) {
-//     if (err) throw err;
-//     connection.query('USE test', function (err) {
-//         if (err) throw err;
-//         connection.query('CREATE TABLE IF NOT EXISTS users('
-//             + 'id INT NOT NULL AUTO_INCREMENT,'
-//             + 'PRIMARY KEY(id),'
-//             + 'name VARCHAR(30)'
-//             +  ')', function (err) {
-//                 if (err) throw err;
-//             });
-//     });
-// });
-
+connection.query('SELECT * FROM Pet', function(err, results) {
+  if(err){
+    console.log(err);
+    return;
+  }
+  console.log(results);
+});
+connection.end();
 // Configuration
 
-app.use(express.bodyParser());
-
-// Main route sends our HTML file
-
-app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
-
-// Update MySQL database
-
-app.post('/users', function (req, res) {
-    connection.query('INSERT INTO users SET ?', req.body,
-        function (err, result) {
-            if (err) throw err;
-            res.send('User added to database with ID: ' + result.insertId);
-        }
-    );
-});
+// app.use(express.bodyParser());
+//
+// // Main route sends our HTML file
+//
+// app.get('/', function(req, res) {
+//     res.sendfile(__dirname + '/index.html');
+// });
+//
+// // Update MySQL database
+//
+// app.post('/users', function (req, res) {
+//     connection.query('INSERT INTO users SET ?', req.body,
+//         function (err, result) {
+//             if (err) throw err;
+//             res.send('User added to database with ID: ' + result.insertId);
+//         }
+//     );
+// });
 
 // Begin listening
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(7002);
+// console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
