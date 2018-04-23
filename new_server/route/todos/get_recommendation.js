fd1b3266452cce573a4fe7addbbe546a9b324798
zhanyan97 <zhuzhanyan97@gmail.com>
2018-04-23 00:15:55 -0500

    fake

--- new_server/route/todos/get_recommendation.js
+++ new_server/route/todos/get_recommendation.js
@@ -7,12 +7,13 @@ const sendError = require('../../helper/sendError');
 
 module.exports = (req, res) => {
   const username = req.params.username;
+  var poid_arr = []
   Promise.resolve(username)
   .then((username) => {
     return get_likedPOID(username) // get POID liked by the user
   })
   .then((data)=>{
-    var poid_arr = []
+    console.log(data)
     for(var index in data){
       poid_arr.push(data[index]["likedPOID"])
     }
@@ -22,32 +23,35 @@ module.exports = (req, res) => {
   //   console.log(data)
   // })
   .then((poid_arr) => {
-    var i = 0;
-    var breedList = []
-    for(i;i<poid_arr.length;i++){
-      var POID = poid_arr[i];
-      console.log("poid")
-      console.log(POID);
-      console.log("----end")
-      var temp = get_breed(POID);
-      console.log(temp);
-      console.log(temp[0]["postBy"])
-      if(temp[0]){
-        console.log("-----");
-        console.log(temp[0]["postBy"]);
-        var temp1 = temp[0]["postBy"];
-        var breed = get_breed_step2(temp1)
-        if(breed[0]){
-          breedList.push(breed[0].breed);
-        }
-      }
-    }
-    return breedList;
+    // var i = 0;
+    // var postByList = []
+    // for(i;i<poid_arr.length;i++){
+    //   var POID = poid_arr[i];
+    //   console.log("poid")
+    //   console.log(POID);
+    //   console.log("----end")
+    //   var temp = get_breed(POID);
+    //   var promise = new Promise(function(resolve, reject) { ... }); 
+    //   console.log("这步走到了没");
+    //   // console.log(temp.data[0]["postBy"])
+    //   console.log("这步走到了没22");
+    //   postByList.push(temp);
+    //   // if(temp[0]){
+    //   //   console.log("-----");
+    //   //   console.log(temp[0]["postBy"]);
+    //   //   var temp1 = temp[0]["postBy"];
+    //   //   var breed = get_breed_step2(temp1)
+    //   //   if(breed[0]){
+    //   //     breedList.push(breed[0].breed);
+    //   //   }
+    //   // }
+    // }
+    // console.log("走完")
+    // return postByList;
   })
-  .then((breedList) => {
-    console.log(breedList)
+  .then((postByList) => {
     res.status(200).send({
-      data: breedList
+        msg:"haha"
     })
   })
   .catch(err => {
